26d24d295a73db5214ebca9b3537af3e1c7b78d5
Raphael Pinto <raphaelcpinto@gmail.com>
2016-08-09 12:12:29 -0300

    update

---
+++ app/pages/add/add.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/add/add.html'
+})
+
+export class AddPage {
+  constructor(public navCtrl: NavController) {
+
+  }
+}
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,56 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {AddPage} from './pages/add/add';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
-export class HomePage {
-  constructor(public navCtrl: NavController) {
 
+export class HomePage {
+  map: any;
+  constructor(public navCtrl: NavController) {
+    this.map = null;
+  }
+  ngOnInit(){
+    this.loadMap();
+  }
+  loadMap(){
+    let options = {timeout: 10000, enableHighAccuracy: true};
+
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+            let mapOptions = {
+                center: latLng,
+                zoom: 19,
+                disableDefaultUI: true,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+
+            this.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+            let marker = new google.maps.Marker({
+              map: this.map,
+              animation: google.maps.Animation.DROP,
+              position: this.map.getCenter()
+            });
+
+            //marker.setIcon("assets/images/ash.png")
+
+            google.maps.event.addListener(this.map, 'click', function(event) {
+                this.placeMarker(event.latLng);
+            });
+
+        },
+
+        (error) => {
+            console.log(error);
+        }, options
+
+    );
+  }
+  add(){
+    alert('teste');
   }
 }
